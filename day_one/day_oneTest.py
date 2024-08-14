b525b759806a909e4439018be05c1dc79ce78f05
BrockPaperScissors <michaeljbrock91@gmail.com>
2024-08-13 22:23:50 -0700

    Review of strings and user input.

--- day_one/day_oneTest.py
+++ day_one/day_oneTest.py
@@ -1,1 +1,17 @@
-print ("Hello world")
+print("Hello World!\nHello World!\nHello World!")
+print("Hello" + " World!")
+
+name = input("What is your name? ")
+
+print(f"Hello {name}")
+
+print("Hello " + name + " " + input("what is your name now? ") + "!")
+
+print("Your name is " + str(len(name)) + " characters long")
+
+print(len(input("What is your favorite word? ")), "letters are in that word")
+
+username = input("What is your username? ")
+length = len(username)
+
+print(length)
