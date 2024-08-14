125d13e9f7df330bc1c4cb9ea9847d3894040cbf
BrockPaperScissors <michaeljbrock91@gmail.com>
2024-08-13 22:29:49 -0700

    day 1 exercise - band name generator

---
+++ day_one/band_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+city = input("What city did you grow up in?\n")
+pet_name = input("What is the best name for a pet?\n")
+
+print("Your band name should be " + city + " " + pet_name + "!")
