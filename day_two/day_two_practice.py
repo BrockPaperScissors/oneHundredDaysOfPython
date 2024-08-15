3bcd5e583f0fa2814decdafa6208599b82e519df
BrockPaperScissors <michaeljbrock91@gmail.com>
2024-08-14 23:14:34 -0700

    day 2 - practice with data types, casting, f-strings, and rounding of numbers

---
+++ day_two/day_two_practice.py
@@ -0,0 +1,17 @@
+# Practice with type checking
+print(type("hello"))
+print(type(23))
+print(type(True))
+print(type(42.092))
+
+# type casting
+print("Number of letters in your name: " + str(len(input("Enter your name: "))))
+
+# rounding takes two arguments, your number and how many places to round to.
+print(round(284.2784, 3))
+
+# f-strings
+
+age = 74
+
+print(f"I am {age} years old")
